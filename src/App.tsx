f3cae669e89e7d5b3af4a0e6478c75d4c1000fe5
Mathieu Tinoco <tinoco.mathieu@gmail.com>
2023-03-12 21:53:51 +0100

    Set up app fundations

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
-import UsersTable from "./components/UsersTable";
+import UsersTable from './components/UserList';
+import UserPosts from './components/UserPosts';
+import AppBody from './components/AppBody';
 
-function App() {
-  return (
-    <UsersTable/>
-  );
-}
+const App = (): JSX.Element => (
+  <div className="App">
+    <BrowserRouter>
+      <AppBody>
+        <Routes>
+          <Route path="/" element={<UsersTable />} />
+          <Route path="/user-posts/:userId" element={<UserPosts />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </AppBody>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
